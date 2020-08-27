dc94c9244f61f545a69396ec6e151f166392e959
Ujjal Acharya <acharyaujjal1@gmail.com>
2020-08-27 17:11:28 +0545

    Update api.js

--- src/utility/api.js
+++ src/utility/api.js
@@ -1,7 +1,7 @@
 export const login = (username, password) => {
   return new Promise((resolve, reject) => {
     return setTimeout(() => {
-      if (username === "biplav@basobaas.com" && password === "12345") {
+      if (username === "ujjal@hotmail.com" && password === "12345") {
         return resolve({ status: 200 });
       }
       return reject({ success: 404 });
